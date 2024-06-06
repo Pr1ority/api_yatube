be13beeee131c300a81d71719118ffa303525122
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-06-06 16:03:18 +0300

    Update

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -40,6 +40,11 @@ class CommentViewSet(viewsets.ModelViewSet):
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super().perform_update(serializer)
 
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super().perform_destroy(instance)
+
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
